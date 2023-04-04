0eea3874c4e8befa49181ade201f00cef8802e9e
ProgramerNila <programernila@gmail.com>
2023-04-04 13:00:06 +0600

    fix route

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,9 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Profile from "../pages/Profile";
 import Registation from "../pages/Registation";
-import Login from "../pages/Login"
+import Login from "../pages/Login";
 import ForgetPassword from "../pages/ForgetPassword";
 import Home from "../pages/Home";
-export const router = createBrowserRouter([
+import RootLayout from "../layout/RootLayout";
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<RootLayout />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/registation" element={<Registation />} />
+      <Route path="/forgotpassword" element={<ForgetPassword />} />
+    </Route>
+  )
+);
+/* export const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
@@ -20,4 +39,4 @@ export const router = createBrowserRouter([
     path: "/forgotpassword",
     element: <ForgetPassword />,
   },
-]);
+]); */
---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import Topbar from '../components/Topbar';
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Topbar/>
+      <Outlet />
+
+      <h2>Footer</h2>
+    </div>
+  )
+}
+
+export default RootLayout
